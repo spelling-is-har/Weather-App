6ccd5ad341d8788516fe70890b416d280508a654
Jonathan Winter <jonathan.winter@hotmail.com>
2026-03-18 21:34:58 +0000

    Create API call to weather On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   src/index.js
    
    Untracked files:
    	.gitignore

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import "./styles.css";
 
-console.log("hello");
+const API_KEY = "D22DDZSZ8J58JTVVUXHMVT3PX";
+
+// https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/london?key=D22DDZSZ8J58JTVVUXHMVT3PX
+
+async function callApi(location) {
+  const response = await fetch(
+    "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" +
+      location +
+      "?key=" +
+      API_KEY,
+  );
+  return response.json();
+}
+callApi("paris").then((data) => {
+  console.log(data);
+});
